e1981a40d6a221e008369cb3b8fbdbd679ed8489
dls105555 <33935637+dls105555@users.noreply.github.com>
2017-12-29 10:16:57 -0600

    Add file where we were looking elements by tag

---
+++ FindElementByTag2.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+#create (open) browser
+driver = webdriver.Firefox()
+driver.get("http://www.humanemulator.net/poligon/input.html")
+inputs = driver.find_elements_by_tag_name("input")
+for input in inputs:
+    name = input.get_attribute("name")
+    print(name)
