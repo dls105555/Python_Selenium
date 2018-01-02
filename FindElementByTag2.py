15a09e96c0e89fc0ba7520c47d2c05961ff880c0
dls105555 <33935637+dls105555@users.noreply.github.com>
2018-01-02 08:34:03 -0600

    My first commit

--- FindElementByTag2.py
+++ FindElementByTag2.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 driver = webdriver.Firefox()
 driver.get("http://www.humanemulator.net/poligon/input.html")
 inputs = driver.find_elements_by_tag_name("input")
+
 for input in inputs:
     name = input.get_attribute("name")
     print(name)
