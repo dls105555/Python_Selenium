25de69d9cf7b8b156b1aead7c69021a95c1308bd
dls105555 <33935637+dls105555@users.noreply.github.com>
2018-01-08 08:40:53 -0600

    test

--- Registration.py
+++ Registration.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from time import sleep
+
 driver = webdriver.Firefox()
 driver.get("https://www.litecart.net/account/create")
 
@@ -15,10 +16,12 @@ select = Select(driver.find_element_by_name("role"))
 # select.select_by_visible_text("text")
 select.select_by_value("Developer")
 
+
 select = Select(driver.find_element_by_name("zone_code"))
 # select.select_by_index(index)
 # select.select_by_visible_text("text")
 select.select_by_visible_text("Illinois")
+
 
 checked = driver.find_element_by_name("newsletter").is_selected()
 if checked == False:
